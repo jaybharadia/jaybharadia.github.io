e990c559ce83b5d157edec0467fb047db7b306c3
root <root@localhost.localdomain>
2020-10-12 23:31:01 +0530

    Update1.1

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ $(function(){
 		horizontalScrolling: false
 	});
 	*/
-	
+
 	$(window).on('load', function(){
-	
+
 		setTimeout( function(){ $('body').addClass('loaded'); } , 1200 );
-	
-		
+
+
 		/*=========================================================================
 			Portfolio Grid
 		=========================================================================*/
@@ -21,7 +21,7 @@ $(function(){
 		grid.shuffle({
 			itemSelector: '.item'
 		});
-		
+
 		$('#portfolio-filters > ul > li > a').on('click', function (e) {
 			e.preventDefault();
 			var groupName = $(this).attr('data-group');
@@ -29,7 +29,7 @@ $(function(){
 			$(this).addClass('active');
 			grid.shuffle('shuffle', groupName );
 		});
-		
+
 		$('a.image-link').magnificPopup({
 			type: 'image',
 			removalDelay: 300,
@@ -38,36 +38,36 @@ $(function(){
 				enabled: true
 			}
 		});
-	
+
 	});
-	
-	
-	
+
+
+
 	/*=========================================================================
 		Links Navigation System
 	=========================================================================*/
 	$('.front-person-links > ul > li > a[data-section]').on('click', function(e){
 		e.preventDefault();
 		var section = $('#' + $(this).data('section'));
-		
-		
+
+
 		if( section.size() != 0 ){
-			
+
 			$('body').addClass('section-show');
-			
+
 			section.addClass('active');
-		
+
 		}
-		
+
 	});
-	
+
 	$('.close-btn').on('click', function(){
 		$('body').removeClass('section-show');
 		$('section.active').removeClass('active');
 	});
-	
-	
-	
+
+
+
 	/*=========================================================================
 		Testimonials Slider
 	=========================================================================*/
@@ -75,11 +75,11 @@ $(function(){
 		singleItem: true
 	});
 
-	
+
 /*=========================================================================
 		Footer Section Adding it to all sections from just one variable dynamically after page loads
 	=========================================================================*/
-	
+
 	var footer_list = document.getElementsByClassName('footer');
 	var footer_content = "<div class='container' >\
 						\
@@ -118,48 +118,49 @@ $(function(){
 						\
 						\
 					</div> ";
-		
+
 		for(var i=0; i < footer_list.length; i++){
 		footer_list[i].innerHTML = footer_content;
 		}
-	
+
 	/*=========================================================================
 		Skill Bar's Percent Initialization from attribute data-percent
 	=========================================================================*/
 	var level_count=0;
 	$('.skill-bar').each(function(){
-		
+
 		var $this = $(this),
 			percent = parseInt( $this.data('percent'), 10 );
 		$this.find('.bar').css('width', percent + '%');
-		
+
 		var percent_label_list = document.getElementsByClassName('percent');
 		percent_label_list[level_count].innerHTML = percent + '%';
 		level_count++;
 	});
-	
-	
-	
-	
+
+
+
+
 	/*=========================================================================
 		Contact Form
 	=========================================================================*/
+	
 	function isJSON(val){
 		var str = val.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
 		return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
 	}
 	$('#contact-form').validator().on('submit', function (e) {
-		
+
 		if (!e.isDefaultPrevented()) {
 			// If there is no any error in validation then send the message
-			
+
 			e.preventDefault();
 			var $this = $(this),
-				
+
 				//You can edit alerts here
 				alerts = {
-				
-					success: 
+
+					success:
 					"<div class='form-group' >\
 						<div class='alert alert-success alert-dismissible' role='alert'> \
 							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
@@ -168,9 +169,9 @@ $(function(){
 							<strong>Message Sent!</strong> We'll be in touch as soon as possible\
 						</div>\
 					</div>",
-					
-					
-					error: 
+
+
+					error:
 					"<div class='form-group' >\
 						<div class='alert alert-danger alert-dismissible' role='alert'> \
 							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
@@ -179,28 +180,28 @@ $(function(){
 							<strong>Error!</strong> Sorry, an error occurred. Try again.\
 						</div>\
 					</div>"
-					
+
 				};
-			
+
 			$.ajax({
-			
+
 				url: 'mail.php',
 				type: 'post',
 				data: $this.serialize(),
 				success: function(data){
-					
+
 					if( isJSON(data) ){
-						
+
 						data = $.parseJSON(data);
-						
+
 						if(data['error'] == false){
-							
+
 							$('#contact-form-result').html(alerts.success);
-							
+
 							$('#contact-form').trigger('reset');
-							
+
 						}else{
-							
+
 							$('#contact-form-result').html(
 							"<div class='form-group' >\
 								<div class='alert alert-danger alert-dismissible' role='alert'> \
@@ -211,14 +212,14 @@ $(function(){
 								</div>\
 							</div>"
 							);
-							
+
 						}
-						
-						
+
+
 					}else{
 						$('#contact-form-result').html(alerts.error);
 					}
-					
+
 				},
 				error: function(){
 					$('#contact-form-result').html(alerts.error);
@@ -226,6 +227,6 @@ $(function(){
 			});
 		}
 	});
-	
-	
+
+
 });
